c72f03dab292d4bcc2eb7235b3245708f732f8f4
Nick <nicholas@littlelives.com>
2015-10-01 11:11:05 +0800

    accept UserMetadata

--- ElasticTranscoder.php
+++ ElasticTranscoder.php
@@ -41,7 +41,7 @@ class AWS_ET {
   * @param array $Playlists info about master playlist
   * @return array | false
   */
-  public static function createJob($input, $outputs, $pipelineId, $outputKeyPrefix = null, $playlists = array()) {
+  public static function createJob($input, $outputs, $pipelineId, $outputKeyPrefix = null, $playlists = array(), $userMetadata = array()) {
     self::resetProps();
     self::$HttpRequestMethod = 'POST';
     self::$Uri = '/2012-09-25/jobs';
@@ -79,6 +79,9 @@ class AWS_ET {
       if (array_key_exists('OutputKeys', $playlists)) {
         $requestBody['Playlists']['OutputKeys'] = $playlists['OutputKeys'];
       }
+    }
+    if (!empty($userMeta)) {
+      $requestBody['UserMetadata'] = $userMeta
     }
     $requestBody['PipelineId'] = $pipelineId;
     $requestBody = json_encode($requestBody);
